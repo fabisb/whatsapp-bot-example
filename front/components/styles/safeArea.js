a6056129aad0829d24e9f099acd178eb88f30839
Fabian <silvabravofabian@gmail.com>
2023-07-19 15:12:55 -0400

    app movil tarea 1

--- front/components/styles/safeArea.js
+++ front/components/styles/safeArea.js
@@ -2,7 +2,6 @@ import { StyleSheet, StatusBar } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: StatusBar.currentHeight,
   },
   pagerView: {
     flex: 1,
